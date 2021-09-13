8aa24299a646fec36b9aeb97a59dd15d37ed66cd
Jack Linhart <iatenine@gmail.com>
2021-09-13 12:53:44 -0600

    basic styling and conditional partials rendering

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.render("../views/home.hbs");
+  res.render("../views/home.hbs", {
+    loggedIn: true,
+  });
 });
 
 module.exports = router;
